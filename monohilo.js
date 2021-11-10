3c5076b3f840870dba4a8f7f0ac99be17dfce30f
Hernán D. Mercado <hernandmf@gmail.com>
2021-11-10 18:24:49 -0500

    Learning how monothread works in node.js

---
+++ monohilo.js
@@ -0,0 +1,8 @@
+console.log("Hola nigga");
+
+setInterval(function () {
+  //...
+  console.log("Sigo activo");
+}, 1000);
+
+console.log("Segundo log");
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+console.log("Hola nigga");
+
+setInterval(function () {
+  //...
+  console.log("Sigo activo");
+}, 1000);
