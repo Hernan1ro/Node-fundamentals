83bc6a6ac522000ab8b7762b9e5e9f71aa408a9f
Hernán D. Mercado <hernandmf@gmail.com>
2021-11-10 21:54:06 -0500

    Installing nodemon and pm

--- entorno.js
+++ entorno.js
@@ -1,4 +1,4 @@
 let nombre = process.env.NOMBRE || "Hernan";
-let web = process.env.WEB || "No tengo web F";
+let web = process.env.WEB || "No tengo web Lol";
 
 console.log(`Hola ${nombre} y mi web es: ${web}`);
--- monohilo.js
+++ monohilo.js
@@ -2,7 +2,7 @@ console.log("Hola nigga");
 
 setInterval(function () {
   //...
-  console.log("Sigo activo");
+  console.log("Sigo activo nigaaaa");
 }, 1000);
 
 console.log("Segundo log");
