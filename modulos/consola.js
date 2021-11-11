1f9ad326af1f9fc7165abca1b8af3de1ee89a226
Hernán D. Mercado <hernandmf@gmail.com>
2021-11-11 14:06:53 -0500

    learning about the console

---
+++ modulos/consola.js
@@ -0,0 +1,46 @@
+// var tabla = [
+//   {
+//     a: 2,
+//     b: "Z",
+//     c: "a",
+//   },
+//   {
+//     a: 2,
+//     b: "Otraletra",
+//   },
+// ];
+
+//console.table
+// console.table(tabla);
+
+// console.log("TAble");
+// console.group("conver");
+// console.log("TAble");
+// console.log("Hola");
+// console.log("Niggaaa");
+// console.groupEnd("conver");
+
+// console.log("Niggaaa x2");
+
+// function function1() {
+//   console.group("Funcion1");
+//   console.log("Esto de función 1");
+//   console.log("Esto tambien");
+//   function2();
+//   console.groupEnd("Funcion1");
+// }
+
+// function function2() {
+//   console.group("Funcion2");
+//   console.log("ahora estamos en la funcion 2");
+//   console.groupEnd("Funcion2");
+// }
+
+// function1();
+
+// console.count
+
+console.count("veces");
+console.count("veces");
+console.countReset("veces");
+console.count("veces");
